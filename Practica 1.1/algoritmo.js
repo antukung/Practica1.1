2379a2b106b90a0f9f516d9be7e1b6e70fc96d7c
antukung <122485403+antukung@users.noreply.github.com>
2023-03-17 11:25:41 -0300

    Add files via upload

---
+++ Practica 1.1/algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector("body").innerHTML =`
+  
+    <h1>Mi primera pagina web</h1>
+    <h2>Frond End</h2>
+    <h3>Backend</h3>
+    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/perritos-de-instagram-1616062929.jpg?crop=0.492xw:0.908xh;0.508xw,0.0917xh&resize=640:*" alt="perros">
+    <img src="imagenes/perro.jpg" alt="perrito"></img>
+
+
+
+
+`;
